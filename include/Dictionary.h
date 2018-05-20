b1fe9da83cee8cc9af717af828a095d5c57c3898
BenjaminDeguerre <benjamin.deguerre@insa-rouen.fr>
2018-05-20 08:54:34 +0200

    translate

---
+++ include/Dictionary.h
@@ -0,0 +1,93 @@
+/* Arthur Hamelin - 10/12/2014 */
+/* Alexandre DUVAL - 22/12/2014 */
+
+#ifndef __DICTIONARY__
+#define __DICTIONARY__
+
+#include <stdio.h>
+
+#include "Word.h"
+#include "BinaryTree.h"
+#include "elementCollection.h"
+#include "subString.h"
+#include "ouvrirFichier.h"
+
+/** 
+ *	\typedef D_Dictionary
+ *	\struct BT_BinaryTree
+ */
+typedef struct D_Dictionary
+{
+	BT_BinaryTree wordsBT;
+	int size;
+} D_Dictionary;
+
+#define D_MEMORY_ERROR 1
+/**
+ *	\fn D_Dictionary D_createDictionary(); 
+ *	\brief permet de create un dictionary
+ *	\return un D_Dictionary
+ */
+D_Dictionary D_createDictionary();
+
+/**
+ *	\fn int D_wordIsIN (W_Word *word, D_Dictionary dictionaryictionary);
+ *	\brief permet de savoir si un mot est contenu dans le dictionary
+ *	\param *word pointeur sur un mot
+ *	\param D_Dictionary un dictionary de mots
+ *	\return int
+ */
+int D_wordIsIN(W_Word *word, D_Dictionary dictionaryictionary);
+
+/**
+ *	\fn int D_size (D_Dictionary dictionaryictionary);
+ *	\brief permet d'get la size du dictionary
+ *	\param dictionary un dictionary
+ *	\return int
+ */
+int D_size(D_Dictionary dictionaryictionary);
+
+/**
+ *	\fn void D_add(W_Word *word, D_Dictionary *dictionary);
+ *	\brief Permet d'add un mot dans un dictionary
+ *	\param *word un pointeur sur un mot
+ *	\param *dictionary un pointeur sur un dictionary
+ *	\return void
+ */
+void D_add(W_Word *word, D_Dictionary *dictionary);
+
+/**
+ *	\fn void D_remove(W_Word *word, D_Dictionary *dictionary);
+ *	\brief Permet de remove un mot d'un dictionary
+ *	\param *word un pointeur sur un mot
+ *	\param *dictionary un pointeur sur un dictionary
+ *	\return void
+ */
+void D_remove(W_Word *word, D_Dictionary *dictionary);
+
+/**
+ *	\fn int D_isEmpty(D_Dictionary *dictionary);
+ *	\brief Permet de savoir si un dictionary est vide
+ *	\param *dictionary un dictionary
+ *	\return int
+ */
+int D_isEmpty(D_Dictionary *dictionary);
+
+/**
+ *	\fn int D_serialize(D_Dictionary dictionaryictionary, FILE* targetFile); 
+ *	\brief ecrit le dictionary dans un file
+ *	\param dictionary un dictionary
+ *	\param *targetFile un pointeur sur un file
+ *	\return int
+ */
+int D_serialize(D_Dictionary dictionaryictionary, FILE *targetFile);
+
+/**
+ *	\fn D_Dictionary D_unserialize(char *pathToFile); 
+ *	\brief lit le dico depuis un file
+ *	\param *pathToFile un pointeur sur le chemin du file
+ *	\return D_Dictionary
+ */
+D_Dictionary D_unserialize(char *pathToFile);
+
+#endif
---
+++ src/Dictionary.c
@@ -0,0 +1,318 @@
+/* Arthur HAMELIN - 10/12/2014 */
+/* Alexandre DUVAL - 31/12/2014 */
+
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <assert.h>
+#include <stdio.h>
+
+#include "Dictionary.h"
+
+/* private */
+W_Word *reduce(W_Word word)
+{
+  if (W_getLength(word) == 1)
+  {
+    return W_createWord("");
+  }
+  else
+  {
+    char *string = subString(W_getChaine(word), 1, W_getLength(word) - 1);
+    W_Word *newWord = W_createWord(string);
+    free(string);
+    return newWord;
+  }
+}
+
+void insert(W_Word *word, BT_BinaryTree *tree)
+{
+  BT_BinaryTree newTree;
+  BT_BinaryTree tmp;
+  N_Node *node;
+  W_Word *wordReduced;
+
+  if (W_getLength(*word) > 0)
+  {
+    wordReduced = reduce(*word);
+    if (BT_isEmpty(*tree))
+    {
+      node = N_createNode(W_getLength(*word) == 1, W_getIthCharacter(*word, 0));
+      *tree = BT_addRoot(BT_createSBT(), BT_createSBT(), node, (ES_copy)N_copy);
+      tmp = BT_getLeftChild(*tree);
+      insert(wordReduced, &tmp);
+      if (!BT_isEmpty(tmp))
+      {
+        BT_setLeftChild(tree, tmp);
+      }
+    }
+    else
+    {
+      if (W_getIthCharacter(*word, 0) > N_getValue(*BT_getRoot(*tree)))
+      {
+        tmp = BT_getRightChild(*tree);
+        insert(word, &tmp);
+        if (!BT_isEmpty(tmp))
+        {
+          BT_setRightChild(tree, tmp);
+        }
+      }
+      else
+      {
+        if (W_getIthCharacter(*word, 0) == N_getValue(*BT_getRoot(*tree)))
+        {
+          N_defineEnd(BT_getRoot(*tree), N_estFin(*BT_getRoot(*tree)) || (W_getLength(*word) == 1));
+          tmp = BT_getLeftChild(*tree);
+          insert(wordReduced, &tmp);
+          if (!BT_isEmpty(tmp))
+          {
+            BT_setLeftChild(tree, tmp);
+          }
+        }
+        else
+        {
+          node = N_createNode(W_getLength(*word) == 1, W_getIthCharacter(*word, 0));
+          newTree = BT_addRoot(BT_createSBT(), *tree node, (ES_copy)N_copy);
+          *tree = newTree;
+          tmp = BT_getLeftChild(*tree);
+          insert(wordReduced, &tmp);
+          if (!BT_isEmpty(tmp))
+          {
+            BT_setLeftChild(tree, tmp);
+          }
+        }
+      }
+    }
+    W_delete(wordReduced);
+  }
+}
+
+void serializeTree(FILE *file, BT_BinaryTree *tree)
+{
+  BT_BinaryTree brother;
+  BT_BinaryTree cadet;
+
+  fprintf(file, "%c\n", N_getValue(*BT_getRoot(*tree)));
+
+  if (N_estFin(*BT_getRoot(*tree)))
+  {
+    fprintf(file, ";\n");
+  }
+
+  if (BT_isEmpty(BT_getLeftChild(*tree)))
+  {
+    fprintf(file, ".\n");
+  }
+  else
+  {
+    cadet = BT_getLeftChild(*tree);
+    serializeTree(file, &cadet);
+  }
+
+  if (BT_isEmpty(BT_getRightChild(*tree)))
+  {
+    fprintf(file, ",\n");
+  }
+  else
+  {
+    brother = BT_getRightChild(*tree);
+    serializeTree(file, &brother);
+  }
+}
+
+int wordIsINR(char *stringToTest, BT_BinaryTree *tree)
+{
+  char stringToTest, adjacentCurrentTree;
+  char *nextStringToTest;
+  BT_BinaryTree tmp;
+  int res;
+
+  assert(!BT_isEmpty(*tree));
+
+  if (strlen(stringToTest) == 0)
+  {
+    res = 0;
+  }
+  else if (strlen(stringToTest) == 1)
+  {
+    stringToTest = stringToTest[0];
+    adjacentCurrentTree = N_getValue(*BT_getRoot(*tree));
+    if (stringToTest > adjacentCurrentTree)
+    {
+      tmp = BT_getRightChild(*tree);
+      if (!BT_isEmpty(tmp))
+      {
+        res = wordIsINR(stringToTest, &tmp);
+      }
+      else
+      {
+        res = -1;
+      }
+    }
+    else if (stringToTest == adjacentCurrentTree)
+    {
+      if (N_estFin(*BT_getRoot(*tree)))
+      {
+        res = 1;
+      }
+      else
+      {
+
+        res = 0;
+      }
+    }
+    else
+    {
+      res = -1;
+    }
+  }
+  else
+  {
+    nextStringToTest = subString(stringToTest, 1, strlen(stringToTest) - 1);
+    stringToTest = stringToTest[0];
+    adjacentCurrentTree = N_getValue(*BT_getRoot(*tree));
+
+    if (stringToTest > adjacentCurrentTree)
+    {
+      tmp = BT_getRightChild(*tree);
+      if (!BT_isEmpty(tmp))
+      {
+        res = wordIsINR(stringToTest, &tmp);
+      }
+      else
+      {
+        res = -1;
+      }
+    }
+    else if (stringToTest == adjacentCurrentTree)
+    {
+      tmp = BT_getLeftChild(*tree);
+      if (!BT_isEmpty(tmp))
+      {
+        res = (wordIsINR(nextStringToTest, &tmp));
+      }
+      else
+      {
+        res = -1;
+      }
+    }
+    else
+    {
+      res = -1;
+    }
+    free(nextStringToTest);
+  }
+  return res;
+}
+
+void unserializeTree(FILE *file, BT_BinaryTree *tree)
+{
+  int LINE_LENGTH = 3;
+  char line[LINE_LENGTH];
+
+  if (fgets(line, LINE_LENGTH, file) != NULL)
+  {
+    if ((line[0] >= 97) && (line[0] <= 122))
+    {
+      if (BT_isEmpty(*tree))
+      {
+        N_Node *N1 = N_createNode(0, line[0]);
+        *tree = BT_addRoot(BT_createSBT(), BT_createSBT(), N1, (ES_copy)N_copy);
+        N_free(N1);
+        unserializeTree(file, tree);
+      }
+      else if (!BT_isEmpty(*tree) && BT_isEmpty(BT_getLeftChild(*tree)) && !BT_isProcessed(tree))
+      {
+        N_Node *N2 = N_createNode(0, line[0]);
+        BT_setLeftChild(tree, BT_addRoot(BT_createSBT(), BT_createSBT(), N2, (ES_copy)N_copy));
+        N_free(N2);
+        BT_BinaryTree tmp1 = BT_getLeftChild(*tree);
+        unserializeTree(file, &tmp1);
+        BT_setLeftChild(tree, tmp1);
+        unserializeTree(file, tree);
+      }
+      else
+
+          if (!BT_isEmpty(*tree) && BT_isEmpty(BT_getRightChild(*tree)))
+      {
+        N_Node *N3 = N_createNode(0, line[0]);
+        BT_setRightChild(tree, BT_addRoot(BT_createSBT(), BT_createSBT(), N3, (ES_copy)N_copy));
+        N_free(N3);
+        BT_BinaryTree tmp2 = BT_getRightChild(*tree);
+        unserializeTree(file, &tmp2);
+        BT_setRightChild(tree, tmp2);
+      }
+    }
+
+    if (line[0] == ';' && line[1] == '\n')
+    {
+      N_defineEnd(BT_getRoot(*tree), 1);
+      unserializeTree(file, tree);
+    }
+
+    if (line[0] == '.' && line[1] == '\n')
+    {
+      BT_setIsProcessed(tree, 1);
+      unserializeTree(file, tree);
+    }
+  }
+}
+
+/* public */
+D_Dictionary D_createDictionary()
+{
+  errno = 0;
+  D_Dictionary dictionary;
+
+  dictionary.wordsBT = BT_createSBT();
+  dictionary.size = 0;
+
+  return dictionary;
+}
+
+int D_wordIsIN(W_Word *word, D_Dictionary dictionary)
+{
+  char *stringToTest = W_getChaine(*word);
+  return wordIsINR(stringToTest, &(dictionary.wordsBT));
+}
+
+int D_size(D_Dictionary dictionary)
+{
+  return dictionary.size;
+}
+
+void D_add(W_Word *word, D_Dictionary *dictionary)
+{
+  insert(word, &((*dictionary).wordsBT));
+  (*dictionary).size = (*dictionary).size + 1;
+}
+
+void D_remove(W_Word *word, D_Dictionary *dictionary)
+{
+}
+
+int D_isEmpty(D_Dictionary *dictionary)
+{
+  return BT_isEmpty((*dictionary).wordsBT);
+}
+
+int D_serialize(D_Dictionary dictionary, FILE *targetFile)
+{
+  serializeTree(targetFile, &(dictionary.wordsBT));
+  return 1;
+}
+
+D_Dictionary D_unserialize(char *pathToFile)
+{
+  D_Dictionary dictionary = D_createDictionary();
+  FILE *fileEntree = fopen(pathToFIle, "r+");
+
+  if (fileEntree == NULL)
+  {
+    fprintf(stderr, "\tERROR : impossible to open %s, check if the file exists and the permission on it.\n\t (Dictionary.c)\n", pathToFIle);
+    exit(1);
+  }
+  unserializeTree(fileEntree, &(dictionary.wordsBT));
+
+  return d;
+}
