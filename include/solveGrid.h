61aeed5da0a3b278a586114c280a238cbd25637e
BenjaminDeguerre <benjamin.deguerre@insa-rouen.fr>
2018-05-20 14:54:43 +0200

    translate

---
+++ include/solveGrid.h
@@ -0,0 +1,41 @@
+/* Michel CRESSANT - 11/12/2014 */
+/* Benjamin Deguerre - 02/01/2015 */
+
+/** \file solveGrid.h
+ *	\brief Contient la fonction qui permet de solve une grille de ruzzle
+ *	\version 1.0
+ *	\date 02/01/2015
+ */
+#ifndef __SOLVE_GRID__
+#define __SOLVE_GRID__
+
+#include <string.h>
+
+#include "AdjacentSquares.h"
+#include "Dictionary.h"
+#include "Grid.h"
+#include "LinkedList.h"
+#include "Word.h"
+#include "isSolution.h"
+
+/**
+ *	\typedef sG_Word
+ *	\brief Contient un mot avec sa value en point dans la grille
+ */
+typedef struct sG_Word {
+  char *word; /**< le mot */
+  int point;  /**< Valeur en point du mot */
+} sG_Word;
+
+/**
+ *	\fn	LL_LinkedList solveGrid(D_Dictionary dictionary, G_Grid
+ *gridToSolve)
+ *	\brief retourne une
+ *	\param dic le dictionary contenant l'ensemble des mots possible
+ *	\param gridToSolve la grille à solve
+ *	\return LL_LinkedList
+ */
+
+LL_LinkedList solveGrid(D_Dictionary dictionary, G_Grid gridToSolve);
+
+#endif
---
+++ src/solveGrid.c
@@ -0,0 +1,88 @@
+/* Michel CRESSANT - 11/12/2014 */
+/* Alexandre DUVAL - 14/12/2014 */
+
+#include "solveGrid.h"
+/* private*/
+char *append(char *s, char c) {
+
+  char *res = malloc(sizeof(char) * strlen(s) + 2);
+  res = strcpy(res, s);
+  int len = strlen(s);
+
+  res[len] = c;
+  res[len + 1] = '\0';
+  return res;
+}
+
+void *sG_copyWord(void *wordToCopy) {
+  char *copy = malloc(sizeof(char) * strlen(wordToCopy) + 1);
+  copy = strcpy(copy, (char *)wordToCopy);
+  return (void *)copy;
+}
+
+void solveGridRecurrent(D_Dictionary dictionary, G_Grid grid,
+                        int usedSquare[G_LENGTH], S_SQUARE square,
+                        char *wordToTest, LL_LinkedList *l,
+                        AS_AdjacentSquares *adjacentSquare) {
+
+  int i, position;
+  S_SQUARE *newSquare;
+  usedSquare[S_getPosition(c)] = 1;
+  char *newWordToTest = append(wordToTest, S_getLetter(square));
+
+  AS_addSquare(adjacentSquare, &square);
+  if (isSolution(&dictionary, newWordToTest) >= 0) {
+    if (isSolution(&dictionary, newWordToTest) == 1) {
+      char point[6];
+      sprintf(point, "%d", AS_countPoints(*adjacentSquare));
+      char *newWord = malloc(sizeof(char) * (strlen(newWordToTest) + 7));
+      newWord[0] = '\0';
+      newWord = strcat(newWord, newWordToTest);
+      newWord = strcat(newWord, " ");
+      newWord = strcat(newWord, point);
+
+      LL_add(l, (void *)newWord, sG_copyWord);
+
+      free(newWord);
+    }
+    LL_LinkedList neighbours = G_getNeighbours(grid, square);
+    LL_LinkedList tmp = neighbours;
+    int compteur = LL_length(neighbours);
+    for (i = 0; i < compteur; i++) {
+      newSquare = (S_SQUARE *)LL_getElement(tmp);
+      position = S_getPosition(*newSquare);
+      if (usedSquare[position] != 1) {
+        int *tmpSquareUsed = malloc(sizeof(int) * G_LENGTH);
+        for (int j = 0; j < G_LENGTH; j++) {
+          tmpSquareUsed[j] = usedSquare[j];
+        }
+        solveGridRecurrent(dictionary, g, tmpSquareUsed, *newSquare,
+                           newWordToTest, l, adjacentSquare);
+        AS_deleteSquare(adjacentSquare);
+        free(tmpSquareUsed);
+      }
+      tmp = LL_getNextList(tmp);
+    }
+    LL_delete(&neighbours, S_delete);
+  }
+  free(newWordToTest);
+}
+
+/* public*/
+LL_LinkedList solveGrid(D_Dictionary dictionary, G_Grid gridToSolve) {
+
+  int i;
+
+  LL_LinkedList l = LL_createLinkedList();
+
+  for (i = 0; i < 16; i++) {
+    AS_AdjacentSquares *adjacentSquare = AS_createAdjacentSquares();
+    S_SQUARE square = G_getSquare(gridToSolve, i);
+    int usedSquare[G_LENGTH] = {0};
+    char *wordToTest = "";
+    solveGridRecurrent(dictionary, gridToSolve, usedSquare, square, wordToTest,
+                       &l, adjacentSquare);
+  }
+
+  return l;
+}
