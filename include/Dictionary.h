51ecca47cb098c1ffca965b9aaeddf9254872660
BenjaminDeguerre <benjamin.deguerre@insa-rouen.fr>
2018-05-22 22:54:28 +0200

    documentation for the dictionary

--- include/Dictionary.h
+++ include/Dictionary.h
@@ -16,7 +16,8 @@
  *	\typedef D_Dictionary
  *	\struct BT_BinaryTree
  */
-typedef struct D_Dictionary {
+typedef struct D_Dictionary
+{
   BT_BinaryTree wordsBT;
   int size;
 } D_Dictionary;
@@ -24,67 +25,67 @@ typedef struct D_Dictionary {
 #define D_MEMORY_ERROR 1
 /**
  *	\fn D_Dictionary D_createDictionary();
- *	\brief permet de create un dictionary
- *	\return un D_Dictionary
+ *	\brief Function to create a dictionary.
+ *	\return D_Dictionary
  */
 D_Dictionary D_createDictionary();
 
 /**
- *	\fn int D_wordIsIN (W_Word *word, D_Dictionary dictionarytionary);
- *	\brief permet de savoir si un mot est contenu dans le dictionary
- *	\param *word pointeur sur un mot
- *	\param D_Dictionary un dictionary de mots
+ *	\fn int D_wordIsIN (W_Word *word, D_Dictionary dictionary);
+ *	\brief Tell if a word is in the dictionary or not.
+ *	\param *word Pointer to the W_Word to check.
+ *	\param dictionary The D_Dictionary to check.
  *	\return int
  */
-int D_wordIsIN(W_Word *word, D_Dictionary dictionarytionary);
+int D_wordIsIN(W_Word *word, D_Dictionary dictionary);
 
 /**
- *	\fn int D_size (D_Dictionary dictionarytionary);
- *	\brief permet d'get la size du dictionary
- *	\param dictionary un dictionary
+ *	\fn int D_size (D_Dictionary dictionary);
+ *	\brief Getter for the dictionary size.
+ *	\param dictionary The D_Dictionary to get the size from.
  *	\return int
  */
-int D_size(D_Dictionary dictionarytionary);
+int D_size(D_Dictionary dictionary);
 
 /**
  *	\fn void D_add(W_Word *word, D_Dictionary *dictionary);
- *	\brief Permet d'add un mot dans un dictionary
- *	\param *word un pointeur sur un mot
- *	\param *dictionary un pointeur sur un dictionary
+ *	\brief Add a word into the dictionary.
+ *	\param *word A pointer to a W_Word to add.
+ *	\param *dictionary A pointer to the dictionary to add the word to.
  *	\return void
  */
 void D_add(W_Word *word, D_Dictionary *dictionary);
 
 /**
  *	\fn void D_remove(W_Word *word, D_Dictionary *dictionary);
- *	\brief Permet de remove un mot d'un dictionary
- *	\param *word un pointeur sur un mot
- *	\param *dictionary un pointeur sur un dictionary
+ *	\brief Remove a word from the dictionary.
+ *	\param *word Pointer to the W_Word to remove.
+ *	\param *dictionary The D_Dictionary to remove the word from.
  *	\return void
  */
 void D_remove(W_Word *word, D_Dictionary *dictionary);
 
 /**
  *	\fn int D_isEmpty(D_Dictionary *dictionary);
- *	\brief Permet de savoir si un dictionary est vide
- *	\param *dictionary un dictionary
+ *	\brief Tells if a dictionary is empty.
+ *	\param *dictionary Pointer to the dictionary to check.
  *	\return int
  */
 int D_isEmpty(D_Dictionary *dictionary);
 
 /**
- *	\fn int D_serialize(D_Dictionary dictionarytionary, FILE* targetFile);
- *	\brief ecrit le dictionary dans un file
- *	\param dictionary un dictionary
- *	\param *targetFile un pointeur sur un file
+ *	\fn int D_serialize(D_Dictionary dictionary, FILE* targetFile);
+ *	\brief Serialize the dictionary to a file.
+ *	\param dictionary The D_Dictionary to write.
+ *	\param *targetFile A pointer to the name of the file to write the dictionary to.
  *	\return int
  */
-int D_serialize(D_Dictionary dictionarytionary, FILE *targetFile);
+int D_serialize(D_Dictionary dictionary, FILE *targetFile);
 
 /**
  *	\fn D_Dictionary D_unserialize(char *pathToFile);
- *	\brief lit le dictionary depuis un file
- *	\param *pathToFile un pointeur sur le chemin du file
+ *	\brief Read a serialized dictionary from a file.
+ *	\param *pathToFile Pointer to the file containing the serialized dictionary.
  *	\return D_Dictionary
  */
 D_Dictionary D_unserialize(char *pathToFile);
