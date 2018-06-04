e2b1f684ff88f46f8069c5f6d19e3a96dc700046
BenjaminDeguerre <benjamin.deguerre@insa-rouen.fr>
2018-06-04 23:01:44 +0200

    up

--- src/createGridToSolve.c
+++ src/createGridToSolve.c
@@ -1,17 +1,17 @@
-#include <string.h>
 #include <assert.h>
+#include <string.h>
 
 #include "createGridToSolve.h"
 
-G_Grid createGridToSolve(char *stringParametersGrid)
-{
+G_Grid createGridToSolve(char *stringParametersGrid) {
 
-	assert(strlen(stringParametersGrid) == (G_LENGTH * 4)); /* 16 fois (CARACTERE + 3*space)*/
-	char *arrayParameters[G_LENGTH];
-	int i;
-	for (i = 0; i < G_LENGTH; i++)
-	{
-		arrayParameters[i] = subString(stringParametersGrid, i * G_COTE, i * G_COTE + G_COTE - 1);
-	}
-	return G_createGrid(arrayParameters);
+  assert(strlen(stringParametersGrid) ==
+         (G_LENGTH * 4)); /* 16 fois (CARACTERE + 3*space)*/
+  char **arrayParameters = malloc(sizeof(char *) * G_LENGTH);
+  int i;
+  for (i = 0; i < G_LENGTH; i++) {
+    arrayParameters[i] =
+        subString(stringParametersGrid, i * G_COTE, i * G_COTE + G_COTE - 1);
+  }
+  return G_createGrid(arrayParameters);
 }
--- src/mainRuzzleSolver.c
+++ src/mainRuzzleSolver.c
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#include "Solution.h"
 #include "solveRuzzle.h"
 
 int main(int argc, char **argv)
@@ -14,14 +15,18 @@ int main(int argc, char **argv)
   // testing
   argv[1] = "T1LDR1  A1  S1WTE1LTN1  C3LTE1  U1  R1  I1  L2WDS1  O1WDP3  S1  ";
   argv[2] = "dictionary.dat";
+
   LL_LinkedList listSolutions = solveRuzzle(argv[1], argv[2]);
-  char *valueArray[LL_length(listSolutions) + 1];
+
+  So_Solution *solution;
   int size = LL_length(listSolutions);
+
   printf("%d solutions\n", size);
+
   for (int i = 0; i < size; i++)
   {
-    valueArray[i] = (char *)LL_getElement(listSolutions);
-    printf("%s\n", valueArray[i]);
+    solution = (So_Solution *)LL_getElement(listSolutions);
+    printf("%s %d\n", So_getString(*solution), So_getValue(*solution));
     listSolutions = LL_getNextList(listSolutions);
   }
   return EXIT_SUCCESS;
