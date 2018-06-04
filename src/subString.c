4e3c9b0201f6beb94eca74fcb1b19418be3207c5
BenjaminDeguerre <benjamin.deguerre@insa-rouen.fr>
2018-06-04 23:02:37 +0200

    formatting

--- src/subString.c
+++ src/subString.c
@@ -1,7 +1,6 @@
 /* Michel CRESSANT - 11/12/2014 */
 /* Benjamin Deguerre - 17/12/2014 */
 
-
 /* #define NDEBUG 
    decommenter pour annuler les tests
    de precondition
@@ -11,30 +10,32 @@
 
 /* partie publique */
 
-
 #include "subString.h"
 #include <assert.h>
 #include <string.h>
 #include <errno.h>
 #include <stdlib.h>
-	
-char *subString(char *string, int begin, int end) {
-	char *res = malloc(sizeof(char)*(end - begin + 2));
-	res[end-begin+1] = '\0';
+
+char *subString(char *string, int begin, int end)
+{
+	char *res = malloc(sizeof(char) * (end - begin + 2));
+	res[end - begin + 1] = '\0';
 	int i;
-	
+
 	assert(end >= begin);
 	assert(end <= strlen(string));
 	assert(begin >= 0);
-	
-	if (res == NULL) {
-   		errno = 1;
-    	exit(errno);
-  	}
 
-	for (i = begin; i <= end; i++){
-		res[i-begin] = string[i];					
+	if (res == NULL)
+	{
+		errno = 1;
+		exit(errno);
 	}
 
-	return res;	
+	for (i = begin; i <= end; i++)
+	{
+		res[i - begin] = string[i];
+	}
+
+	return res;
 }
